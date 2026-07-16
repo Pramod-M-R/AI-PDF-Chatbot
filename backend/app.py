f648b8330adbe1e39eb00e5f96969c2f73b0b0ee
Pramod <pramod@Pramods-MacBook-Pro.local>
2026-07-16 07:01:29 +0530

    Fix Railway backend and frontend API

--- backend/app.py
+++ backend/app.py
@@ -26,7 +26,7 @@ app.add_middleware(
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     "https://rag-chatbot-by-pramod.netlify.app",
-    ]
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
